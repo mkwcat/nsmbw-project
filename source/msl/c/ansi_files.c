db146d55f16727ab99a2e925aaaaa154aa1f93b2
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-05-08 01:30:21 -0400

    msl: Add symbol for stdin, stdout, stderr

---
+++ source/msl/c/ansi_files.c
@@ -0,0 +1,11 @@
+// ansi_files.c
+// NSMBW .data: 0x80350D58 - 0x80350E98
+
+#include <stdio.h>
+
+EXTERN_C_START
+
+[[nsmbw_data(0x80350D58)]]
+files __files{};
+
+EXTERN_C_END
--- source/msl/c/stdio.h
+++ source/msl/c/stdio.h
@@ -15,7 +15,7 @@ typedef unsigned long fpos_t;
 #define set_error(file)                                                                            \
     do {                                                                                           \
         (file)->file_state.error = 1;                                                              \
-        (file)->buffer_length = 0;                                                                 \
+        (file)->buffer_length    = 0;                                                              \
     } while (0)
 
 enum __file_kinds {
@@ -32,19 +32,19 @@ enum __file_orientation {
 };
 
 typedef struct _file_modes {
-    unsigned int open_mode : 2;
-    unsigned int io_mode : 3;
-    unsigned int buffer_mode : 2;
-    unsigned int file_kind : 3;
+    unsigned int open_mode        : 2;
+    unsigned int io_mode          : 3;
+    unsigned int buffer_mode      : 2;
+    unsigned int file_kind        : 3;
     unsigned int file_orientation : 2;
-    unsigned int binary_io : 1;
+    unsigned int binary_io        : 1;
 } file_modes;
 
 enum __io_modes {
-    __read = 1,
-    __write = 2,
+    __read       = 1,
+    __write      = 2,
     __read_write = 3,
-    __append = 4,
+    __append     = 4,
 };
 
 enum __io_states {
@@ -61,8 +61,8 @@ enum __io_results {
 };
 
 typedef struct _file_states {
-    unsigned int io_state : 3;
-    unsigned int free_buffer : 1;
+    unsigned int  io_state    : 3;
+    unsigned int  free_buffer : 1;
     unsigned char eof;
     unsigned char error;
 } file_states;
@@ -70,35 +70,35 @@ typedef struct _file_states {
 typedef void (*__idle_proc)(void);
 typedef int (*__pos_proc)(__file_handle file, fpos_t* position, int mode, __idle_proc idle_proc);
 typedef int (*__io_proc)(
-  __file_handle file, unsigned char* buff, size_t* count, __idle_proc idle_proc
+    __file_handle file, unsigned char* buff, size_t* count, __idle_proc idle_proc
 );
 typedef int (*__close_proc)(__file_handle file);
 
 typedef struct _FILE {
     SIZE_ASSERT(0x50);
 
-    /* 0x00 */ __file_handle handle;
-    /* 0x04 */ file_modes file_mode;
-    /* 0x08 */ file_states file_state;
-    /* 0x0C */ unsigned char is_dynamically_allocated;
-    /* 0x0D */ char char_buffer;
-    /* 0x0E */ char char_buffer_overflow;
-    /* 0x0F */ char ungetc_buffer[2];
-    /* 0x12 */ wchar_t ungetc_wide_buffer[2];
-    /* 0x18 */ unsigned long position;
+    /* 0x00 */ __file_handle  handle;
+    /* 0x04 */ file_modes     file_mode;
+    /* 0x08 */ file_states    file_state;
+    /* 0x0C */ unsigned char  is_dynamically_allocated;
+    /* 0x0D */ char           char_buffer;
+    /* 0x0E */ char           char_buffer_overflow;
+    /* 0x0F */ char           ungetc_buffer[2];
+    /* 0x12 */ wchar_t        ungetc_wide_buffer[2];
+    /* 0x18 */ unsigned long  position;
     /* 0x1C */ unsigned char* buffer;
-    /* 0x20 */ unsigned long buffer_size;
+    /* 0x20 */ unsigned long  buffer_size;
     /* 0x24 */ unsigned char* buffer_ptr;
-    /* 0x28 */ unsigned long buffer_length;
-    /* 0x2C */ unsigned long buffer_alignment;
-    /* 0x30 */ unsigned long save_buffer_length;
-    /* 0x34 */ unsigned long buffer_position;
-    /* 0x38 */ __pos_proc position_fn;
-    /* 0x3C */ __io_proc read_fn;
-    /* 0x40 */ __io_proc write_fn;
-    /* 0x44 */ __close_proc close_fn;
-    /* 0x48 */ __idle_proc idle_fn;
-    /* 0x4C */ struct _FILE* next_file;
+    /* 0x28 */ unsigned long  buffer_length;
+    /* 0x2C */ unsigned long  buffer_alignment;
+    /* 0x30 */ unsigned long  save_buffer_length;
+    /* 0x34 */ unsigned long  buffer_position;
+    /* 0x38 */ __pos_proc     position_fn;
+    /* 0x3C */ __io_proc      read_fn;
+    /* 0x40 */ __io_proc      write_fn;
+    /* 0x44 */ __close_proc   close_fn;
+    /* 0x48 */ __idle_proc    idle_fn;
+    /* 0x4C */ struct _FILE*  next_file;
 } FILE;
 
 typedef struct _files {
@@ -107,6 +107,12 @@ typedef struct _files {
     FILE _stderr;
     FILE empty;
 } files;
+
+extern files __files;
+
+#define stdin (&__files._stdin)
+#define stdout (&__files._stdout)
+#define stderr (&__files._stderr)
 
 #define _IONBF 0
 #define _IOLBF 1
