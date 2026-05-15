206b4732cc97530632ecddf3720a4f4d15e6c949
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-05-14 23:37:43 -0400

    Switch linker to lld

--- source/msl/c/include/assert.h
+++ source/msl/c/include/assert.h
@@ -6,10 +6,10 @@ EXTERN_C_START
 void OSPanic(const char* file, int line, const char* format, ...);
 
 #ifdef NDEBUG
-#  define assert(cond) ((void) 0)
+#  define assert(...) ((void) 0)
 #else
-#  define assert(cond)                                                                             \
-      ((cond) || (OSPanic(__FILE_NAME__, __LINE__, "Failed assertion %s", #cond), 0))
+#  define assert(...)                                                                              \
+      ((__VA_ARGS__) || (OSPanic(__FILE_NAME__, __LINE__, "Failed assertion %s", #__VA_ARGS__), 0))
 #endif
 
 EXTERN_C_END
--- source/wiimj2d/state/s_State.h
+++ source/wiimj2d/state/s_State.h
@@ -6,20 +6,20 @@
     void executeState_##_name();                                                                   \
     void initializeState_##_name();                                                                \
     void finalizeState_##_name();                                                                  \
-    static constinit inline const sFStateID_c<_class> StateID_##_name                              \
-    {                                                                                              \
-        #_class "::StateID_" #_name, &_class::initializeState_##_name,                             \
-          &_class::executeState_##_name, &_class::finalizeState_##_name                            \
+    static constinit inline const char StateName_##_name[] = #_class "::StateID_" #_name;          \
+    static constinit inline const sFStateID_c<_class> StateID_##_name {                            \
+        StateName_##_name, &_class::initializeState_##_name, &_class::executeState_##_name,        \
+            &_class::finalizeState_##_name                                                         \
     }
 
 #define sState_Virtual(_class, _name)                                                              \
     virtual void executeState_##_name();                                                           \
     virtual void initializeState_##_name();                                                        \
     virtual void finalizeState_##_name();                                                          \
-    static constinit inline const sFStateID_c<_class> StateID_##_name                              \
-    {                                                                                              \
-        #_class "::StateID_" #_name, &_class::initializeState_##_name,                             \
-          &_class::executeState_##_name, &_class::finalizeState_##_name                            \
+    static constinit inline const char StateName_##_name[] = #_class "::StateID_" #_name;          \
+    static constinit inline const sFStateID_c<_class> StateID_##_name {                            \
+        StateName_##_name, &_class::initializeState_##_name, &_class::executeState_##_name,        \
+            &_class::finalizeState_##_name                                                         \
     }
 
 #define sState_Extern(_address, _class, _name)                                                     \
