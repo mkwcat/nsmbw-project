8f7d78d7c05cd7aaf68272298004ba7626d042f1
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-05-15 21:57:22 -0400

    dEventOpeningTitle_c: Show hardcoded version msg

--- source/wiimj2d/d_bases/d_EventOpeningTitle.cpp
+++ source/wiimj2d/d_bases/d_EventOpeningTitle.cpp
@@ -6,18 +6,44 @@
 #include "d_bases/d_s_restart_crsin.h"
 #include "d_bases/d_s_stage.h"
 #include "d_profile/d_profile.h"
+#include "d_system/d_game_common.h"
 #include "d_system/d_info.h"
 
+[[nsmbw(0x80781D00)]]
+bool dEventOpeningTitle_c::createLayout();
+
+[[nsmbw(0x80781C50)]]
+fBase_c::PACK_RESULT_e dEventOpeningTitle_c::create() {
+    if (m_created) {
+        return PACK_RESULT_e::SUCCEEDED;
+    }
+
+    if (!createLayout()) {
+        return PACK_RESULT_e::NOT_READY;
+    }
+    m_visible = m_created = true;
+
+    m_tE3VerCheck->SetVisible(true);
+    m_rootPane->SetVisible(false);
+
+    nw4r::math::VEC2 scale;
+    dGameCom::DispSizeScale(scale);
+    m_nProportionC->SetScale(scale);
+
+    m_tE3VerCheck->SetString(L"mkwcat-nsmbw v0.6.0", 0);
+
+    return PACK_RESULT_e::SUCCEEDED;
+}
+
 [[nsmbw(0x80782340)]]
-void dEventOpeningTitle_c::executeState_PressButtonExitAnimeEndWait()
-{
+void dEventOpeningTitle_c::executeState_PressButtonExitAnimeEndWait() {
     if (dScStage_c::m_gameMode == dScStage_c::GAME_MODE_e::UNKNOWN_2 && m_layout.isAnime()) {
         return;
     }
 
     if (dScStage_c::m_gameMode == dScStage_c::GAME_MODE_e::UNKNOWN_2) {
         return dScStage_c::setNextScene(
-          dProf::GAME_SETUP, 0, dScStage_c::Exit_e(0), dFader_c::fader_type_e::UNKNOWN_5
+            dProf::GAME_SETUP, 0, dScStage_c::Exit_e(0), dFader_c::fader_type_e::UNKNOWN_5
         );
     }
 
--- source/wiimj2d/d_bases/d_EventOpeningTitle.h
+++ source/wiimj2d/d_bases/d_EventOpeningTitle.h
@@ -2,8 +2,25 @@
 
 #include "d_system/d_base.h"
 #include "d_system/d_lytbase.h"
+#include "d_system/d_lyttextbox.h"
+#include "nw4r/lyt/Pane.h"
+#include "state/s_StateMgrDefault.h"
 
 class dEventOpeningTitle_c : public dBase_c {
+public:
+    // Virtual Methods
+    // ^^^^^^
+
+    /* 0x80781C50 */
+    PACK_RESULT_e create() override;
+
+public:
+    // Instance Methods
+    // ^^^^^^
+
+    /* 0x80781D00 */
+    bool createLayout();
+
 public:
     // State ID Methods
     // ^^^^^^
@@ -15,5 +32,15 @@ public:
     // Instance Variables
     // ^^^^^^
 
-    /* 0x070 */ LytBase_c m_layout;
+    /* 0x070 */ LytBase_c                                m_layout;
+    /* 0x208 */ sStateMgrDefault_c<dEventOpeningTitle_c> m_stateMgr;
+    /* 0x244 */ nw4r::lyt::Pane*                         m_rootPane;
+    /* 0x248 */ LytTextBox_c*                            m_tE3VerCheck;
+    /* 0x24C */ nw4r::lyt::Pane*                         m_nProportionC;
+
+    FILL(0x250, 0x274);
+
+    /* 0x274 */ bool m_created;
+    /* 0x275 */ bool m_visible;
+    /* 0x276 */ bool m_pressButton;
 };
